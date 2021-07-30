1bf7b7062a8dad5cab2df92cde96676efe1d7bc1
Yingyord, First <phuriy2@illinois.edu>
2021-07-30 14:01:53 +0700

    Relocated files

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, () => {
-    console.log("Application started and listening on port 3000");
+    console.log(`Application started and listening on port 3000`);
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 })
